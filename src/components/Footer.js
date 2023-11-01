e57c7e40ef1b0533df5f17432c6e3be4a2c6ca1a
oskar lindqvist <lindqvist_11@hotmail.com>
2023-11-01 21:50:24 +0700

    new footerboxes component added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 
 import logotype from '../assets/images/logotype-white.svg'
 import backgroundlineswhiteright from '../assets/images/s-line-right.svg'
+import FooterBoxes from './FooterBoxes'
 
 const Footer = () => {
   return (
@@ -14,34 +15,10 @@ const Footer = () => {
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat obcaecati<br />voluptas voluptates! Voluptates laborum<br />nam ratione minus necessitatibus, iure praesentium.</p>
                 </div>
                 <div className="footer-objects">
-                    <div className="footer-company">
-                        <h3>Company</h3>
-                        <Link to="/">About</Link>
-                        <Link to="/">Features</Link>
-                        <Link to="/">Works</Link>
-                        <Link to="/">Career</Link>
-                    </div>
-                    <div className="footer-Help">
-                        <h3>Help</h3>
-                        <Link to="/">Customer Support</Link>
-                        <Link to="/">Delivery Details</Link>
-                        <Link to="/">Terms & Conditions</Link>
-                        <Link to="/">Privacy Policy</Link>
-                    </div>
-                    <div className="footer-Resources">
-                        <h3>Resources</h3>
-                        <Link to="/">Free eBooks</Link>
-                        <Link to="/">Development Tutorial</Link>
-                        <Link to="/">How to - Blog</Link>
-                        <Link to="/">Youtube Playlist</Link>
-                    </div>
-                    <div className="footer-Link">
-                        <h3>Link</h3>
-                        <Link to="/">Free eBooks</Link>
-                        <Link to="/">Development Tutorial</Link>
-                        <Link to="/">How to - Blog</Link>
-                        <Link to="/">Youtube Playlist</Link>
-                    </div>
+                    <FooterBoxes section="company" />
+                    <FooterBoxes section="help" />
+                    <FooterBoxes section="resources" />
+                    <FooterBoxes section="/" />
                 </div>
             </div>
         </footer>
---
+++ src/components/FooterBoxes.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const FooterBoxes = ({ section }) => {
+  switch (section) {
+    case 'company':
+      return (
+        <div className="footer-company">
+          <h3>Company</h3>
+          <Link to="/">About</Link>
+          <Link to="/">Features</Link>
+          <Link to="/">Works</Link>
+          <Link to="/">Career</Link>
+        </div>
+      )
+    case 'help':
+      return (
+        <div className="footer-Help">
+          <h3>Help</h3>
+          <Link to="/">Customer Support</Link>
+          <Link to="/">Delivery Details</Link>
+          <Link to="/">Terms & Conditions</Link>
+          <Link to="/">Privacy Policy</Link>
+        </div>
+      )
+    case 'resources':
+      return (
+        <div className="footer-Resources">
+          <h3>Resources</h3>
+          <Link to="/">Free eBooks</Link>
+          <Link to="/">Development Tutorial</Link>
+          <Link to="/">How to - Blog</Link>
+          <Link to="/">Youtube Playlist</Link>
+        </div>
+      )
+    default:
+        return (
+            <div className="footer-Link">
+              <h3>Link</h3>
+              <Link to="/">Free eBooks</Link>
+              <Link to="/">Development Tutorial</Link>
+              <Link to="/">How to - Blog</Link>
+              <Link to="/">Youtube Playlist</Link>
+            </div>
+          )
+  }
+}
+
+export default FooterBoxes;
